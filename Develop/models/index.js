92b6bb0620a411a15962eaceec8b52e3a122e1c0
trswork <87744214+trswork@users.noreply.github.com>
2021-12-31 00:54:47 -0500

    update

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -3,14 +3,24 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const bcrypt = require('bcrypt');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
